6b00f18278a9ab46b51ad0350d945cdef8c0cbe9
w4zEl <proximitytrigger@gmail.com>
2024-07-09 17:06:17 -0400

    Expand navbar.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,25 @@
+<script>
+	import '../app.css';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button } from 'flowbite-svelte';
+</script>
+
 <!-- Header -->
-<nav>
-    <a href="/">Home</a>
-</nav>
+<Navbar color="form">
+	<NavBrand href="/">
+		<img src="https://via.placeholder.com/50" alt="Logo" /> <!-- Put logo here -->
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			GooseGooseGo
+		</span>
+	</NavBrand>
+	<div class="flex md:order-2">
+		<Button size="sm">Get started</Button>
+		<NavHamburger />
+	</div>
+	<NavUl>
+		<!-- Other nav links -->
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/about">About</NavLi>
+		<NavLi href="/contact-us">Contact Us</NavLi>
+	</NavUl>
+</Navbar>
 <slot></slot>
