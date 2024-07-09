d645993594f34c0d6658f74cce9b8788111e36c8
Jack Li <lijack466@gmail.com>
2024-07-08 22:46:30 -0400

    Added filled test-sample.out

--- app/sql/test-sample.sql
+++ app/sql/test-sample.sql
@@ -8,3 +8,19 @@ SELECT * FROM Reviews WHERE restaurant_id = 2;
 SELECT * FROM Favorites WHERE user_id = 1;
 SELECT * FROM Restaurant WHERE cuisine = 'chinese'; 
 SELECT * FROM Discount WHERE restaurant_id = 1;
+
+SELECT username, email FROM Users WHERE user_id = 1;
+
+SELECT * FROM Items WHERE restaurant_id = 3;
+
+INSERT INTO Reviews (restaurant_id, user_id, rating, comments, timestamp)
+VALUES (2, 3, 4, 'Tuna...', '2024-07-01 00:00:00');
+SELECT * FROM Reviews WHERE restaurant_id = 2;
+
+INSERT INTO Favorites (user_id, restaurant_id)
+VALUES (4, 1);
+SELECT * FROM Favorites WHERE user_id = 1;
+
+SELECT * FROM Restaurant WHERE cuisine = 'chinese';
+
+SELECT * FROM Discount WHERE restaurant_id = 1;
