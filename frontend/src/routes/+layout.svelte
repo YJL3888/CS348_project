d4107b0f75959c460543b04f3adc2e50de71d851
Max Lu <maxzylu@gmail.com>
2024-07-09 18:05:58 -0400

    added some styling to home page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,25 +1,30 @@
 <script>
 	import '../app.css';
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button } from 'flowbite-svelte';
-</script>
-
-<!-- Header -->
-<Navbar color="form">
+  </script>
+  
+  <!-- Header -->
+  <Navbar>
 	<NavBrand href="/">
-		<img src="https://via.placeholder.com/50" class="me-3 h-6 sm:h-9" alt="Logo" /> <!-- Put logo here -->
-		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-			GooseGooseGo
-		</span>
+	  <img src="https://i.imgur.com/u7ESt9S.png" style="margin-right: 1rem; height: 36px;" alt="Logo" />
+	  <span style="color: #4C8C2B; font-size: 1.25rem; font-weight: bold;">
+		GooseGooseGo
+	  </span>
 	</NavBrand>
 	<div class="flex md:order-2">
-		<Button size="sm">Get started</Button>
-		<NavHamburger />
+	  <Button size="sm" style="background-color: #4C8C2B; color: white; padding: 0.5rem 1rem; border-radius: 0.5rem;">
+		Get started
+	  </Button>
+	  <NavHamburger />
 	</div>
 	<NavUl>
-		<!-- Other nav links -->
-		<NavLi href="/">Home</NavLi>
-		<NavLi href="/about">About</NavLi>
-		<NavLi href="/contact-us">Contact Us</NavLi>
+	  <NavLi href="/" style="color: #4C8C2B; margin-right: 1rem;">Home</NavLi>
+	  <NavLi href="/about" style="color: #4C8C2B; margin-right: 1rem;">About</NavLi>
+	  <NavLi href="/contact-us" style="color: #4C8C2B; margin-right: 1rem;">Contact Us</NavLi>
 	</NavUl>
-</Navbar>
-<slot></slot>
+  </Navbar>
+  <div style="margin-left: 12rem; margin-right: 12rem;">
+	<br>
+	<slot></slot>
+  </div>
+  
