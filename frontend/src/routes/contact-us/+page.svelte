8e150da1ba752279907a33abdbde1ba176bb41b0
Jason Cheng <jcheng3927@gmail.com>
2024-07-09 23:00:54 -0400

    contact us

---
+++ frontend/src/routes/contact-us/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Reach out to the dedicated and distinguished GooseGooseGo team here:</h1>
+
+<ul>
+  <li><strong>Jason Cheng:</strong> <a href="mailto:j25cheng@uwaterloo.ca">j25cheng@uwaterloo.ca</a></li>
+  <li><strong>Jack Li:</strong> <a href="mailto:j279li@uwaterloo.ca">j279li@uwaterloo.ca</a></li>
+  <li><strong>Jason Liu:</strong> <a href="mailto:yj7liu@uwaterloo.ca">yj7liu@uwaterloo.ca</a></li>
+  <li><strong>Max Lu:</strong> <a href="mailto:m22lu@uwaterloo.ca">m22lu@uwaterloo.ca</a></li>
+  <li><strong>Howard Ou:</strong> <a href="mailto:hlou@uwaterloo.ca">hlou@uwaterloo.ca</a></li>
+</ul>
