f60238f6b0470a12f3555a1f20220566757cdb91
Jack Li <lijack466@gmail.com>
2024-07-09 20:15:38 -0400

    updated test-production again

--- sql/test-production.sql
+++ sql/test-production.sql
@@ -5,40 +5,32 @@ USE prod_data;
 -- Show tables
 SHOW TABLES;
 
+-- Create a new user
+INSERT IGNORE INTO Users (username, password, email) VALUES ('howard', '006cc76799242f7d99f74d80fb30bbd0ddcaa3bba5798caca638071482774527', 'hlou@uwaterloo.ca');
+
 -- Select username and email from Users where user_id is 1
 SELECT username, email FROM Users WHERE user_id = 1 LIMIT 10;
 
--- Select all items from Items where restaurant_id is 3
-SELECT * FROM Items WHERE restaurant_id = 3 LIMIT 10;
-
--- Select all reviews from Reviews where restaurant_id is 2
-SELECT * FROM Reviews WHERE restaurant_id = 2 LIMIT 10;
-
--- Select all favorites from Favorites where user_id is 1
-SELECT * FROM Favorites WHERE user_id = 1 LIMIT 10;
-
--- Select all restaurants where cuisine is 'chinese'
-SELECT * FROM Restaurant WHERE cuisine = 'chinese' LIMIT 10;
-
--- Select all discounts where restaurant_id is 1
-SELECT * FROM Discount WHERE restaurant_id = 1 LIMIT 10;
+-- Select all items from Items where restaurant_id is 34
+SELECT * FROM Items WHERE restaurant_id = 34 LIMIT 10;
 
 -- Insert a review into Reviews
-INSERT INTO Reviews (restaurant_id, user_id, rating, comments, timestamp)
-VALUES (2, 3, 4, 'Tuna...', '2024-07-01 00:00:00');
+INSERT IGNORE INTO Reviews (restaurant_id, user_id, rating, comments, timestamp)
+VALUES (33, 1, 4, 'The Margherita pizza was delicious, with a perfect balance of cheese and tomato sauce.', '2024-06-06 12:30:00');
 
--- Select all reviews from Reviews where restaurant_id is 2
-SELECT * FROM Reviews WHERE restaurant_id = 2 LIMIT 10;
+-- Select all reviews from Reviews where restaurant_id is 33
+SELECT * FROM Reviews WHERE restaurant_id = 33 LIMIT 10;
 
 -- Insert a favorite into Favorites
-INSERT INTO Favorites (user_id, restaurant_id)
-VALUES (4, 1);
+INSERT IGNORE INTO Favorites (user_id, restaurant_id)
+VALUES (1, 45);
 
 -- Select all favorites from Favorites where user_id is 1
 SELECT * FROM Favorites WHERE user_id = 1 LIMIT 10;
 
--- Select all restaurants where cuisine is 'chinese'
-SELECT * FROM Restaurant WHERE cuisine = 'chinese' LIMIT 10;
+-- Select all restaurants where cuisine is 'Pizza'
+SELECT * FROM Restaurants WHERE cuisine = 'Pizza' LIMIT 10;
 
 -- Select all discounts where restaurant_id is 1
 SELECT * FROM Discount WHERE restaurant_id = 1 LIMIT 10;
+S
