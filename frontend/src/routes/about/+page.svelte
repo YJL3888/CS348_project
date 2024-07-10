22aa3ff65d31bebb3aa49e8c59e1d350bd54b51e
Max Lu <maxzylu@gmail.com>
2024-07-09 23:57:34 -0400

    updated about

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,12 +1,11 @@
-
 <p>
   GooseGooseGo is an application designed to enhance the dining experience for students and food enthusiasts at Waterloo. It uses a custom dataset collected primarily from restaurants near campus (either scraped from the web or inputted manually) to track their menus, prices, and discounts to create the best application experience for users.
 </p>
-
+<br>
 <p>
   For example, they would be able to see that Gol’s Noodles has a 15% storewide discount every Tuesday which is information that is typically quite hard to access. Users can also view detailed information about restaurant items and leave reviews to rate their dining experiences to help others users when choosing their next gastronomic experience.
 </p>
-
+<br>
 <p>
   The application also provides personalized recommendations and generates reports on popular restaurants based on the users’ reviews and history on them. The development team (GooseGooseGeese) serves as the administrators, ensuring data integrity and performing necessary updates.
 </p>
