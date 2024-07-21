6fad33988c0da51469a66a00f3cc86e307eb3304
w4zEl <proximitytrigger@gmail.com>
2024-07-21 01:00:09 -0400

    Make config.py only load .env variables, usage is left to other modules.

--- backend/config.py
+++ backend/config.py
@@ -1,14 +1,3 @@
-import os
 import dotenv
 
 dotenv.load_dotenv()
-
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-ENV = os.getenv('ENV')
-
-#test
-
-if ENV == "sample":
-    DB_NAME = "sample_data"
-else:
-    DB_NAME = "prod_data"
