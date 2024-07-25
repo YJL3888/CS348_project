77b2af639e3fa23a32f7a2d9caddb06ea428f766
Max Lu <maxzylu@gmail.com>
2024-07-25 03:40:13 -0400

    some styling for dark mode

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -86,7 +86,7 @@
 			class="ml-2 mr-2 h-16 w-16 rounded-full"
 			alt="Logo"
 		/>
-		<span class="font-sans text-xl font-semibold text-[#4C8C2B]">GooseGooseGo</span>
+		<P class="font-sans text-xl font-semibold text-[#4C8C2B]">GooseGooseGo</P>
 	</NavBrand>
 	<!-- Search Bar -->
 	<form class="mr-6 flex w-2/5 justify-start" on:submit|preventDefault={handleSearch}>
@@ -125,7 +125,7 @@
 		<!-- Add Checkbox for Favourites -->
 		<div class="ml-2 flex items-center">
 			<Checkbox id="favourites-checkbox" bind:checked={showFavourites} />
-			<label for="favourites-checkbox" class="ml-1 text-sm text-gray-700">Favourites</label>
+			<P for="favourites-checkbox" class="ml-1 text-sm text-gray-700">Favourites</P>
 		</div>
 	</form>
 	<div class="flex md:order-2">
