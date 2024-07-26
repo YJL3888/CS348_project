5c15b040faf952c51c5851d9df7fa1d2238105e6
w4zEl <proximitytrigger@gmail.com>
2024-07-25 20:20:33 -0400

    Remove redundant await.

--- frontend/src/routes/api/comment/+server.ts
+++ frontend/src/routes/api/comment/+server.ts
@@ -2,7 +2,7 @@ import type { RequestHandler } from "./$types";
 import { PUBLIC_BACKEND_BASE } from "$env/static/public";
 
 export const POST: RequestHandler = async ({ fetch, request, cookies }) => {
-    return await fetch(PUBLIC_BACKEND_BASE + "/restaurants/comment", {
+    return fetch(PUBLIC_BACKEND_BASE + "/restaurants/comment", {
         method: 'POST',
         body: await request.formData(),
         headers: {
